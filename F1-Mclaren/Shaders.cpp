c53e774301fab4b74162d01534759f10aeaab854
L-Y-V-C <yamil.valenzuela.96@gmail.com>
2026-06-18 03:21:04 -0500

    feat(animation): add car animation based on steps
    add car animation using instructions of movement to maintain physics

--- F1-Mclaren/Shaders.cpp
+++ F1-Mclaren/Shaders.cpp
@@ -10,88 +10,29 @@
 const char* vertex_shader="#version 330 core\n"
 "layout (location=0) in vec3 inVec;\n"
 "layout (location=1) in vec2 inUVs;\n"
-"layout (location=2) in vec3 inNormal;\n"
-
 "out vec2 inOutUV;\n"
-"out vec3 FragPos;\n"
-"out vec3 Normal;\n"
-
 "uniform mat4 model;\n"
 "uniform mat4 view;\n"
 "uniform mat4 projection;\n"
-
-
 "void main(){\n"
 "	gl_Position=projection*view*model*vec4(inVec.xyz,1.0f);\n"
-"	FragPos = vec3(model * vec4(inVec, 1.0f));\n"
-
-"	Normal = mat3(transpose(inverse(model))) * inNormal;\n"
 "	inOutUV=inUVs;\n"
 "}\n";
 
 const char* fragment_shader="#version 330 core\n"
 "in vec2 inOutUV;\n"
-"in vec3 FragPos;\n"\
-"in vec3 Normal;\n"
 "out vec4 outColor;\n"
-
-// --- Datos del Material (.mtl) ---
-"uniform vec3 Ka;\n" // Color Ambiente
-"uniform vec3 Kd;\n" // Color Difuso
-"uniform vec3 Ks;\n" // Color Especular
-"uniform float Ns;\n" // Brillo (Shininess)
-
-// --- Datos de la Luz y Cámara ---
-"uniform vec3 lightPos;\n"   // Posición de la luz en el mundo
-"uniform vec3 lightColor;\n" // Color de la luz (ej: vec3(1.0))
-"uniform vec3 viewPos;\n"    // Posición de la cámara/usuario
+"uniform vec3 ouCol;\n"
 "uniform sampler2D ouTexture;\n"
 "uniform bool useTexture;\n"
-"uniform vec3 ouColor;\n" 
-
-"uniform bool isLightCube;\n"
-
 "void main(){\n"
-"   vec3 norm = normalize(Normal);\n"
-"   vec3 lightDir = normalize(lightPos - FragPos);\n"
-
-"   if(isLightCube){\n"
-"       outColor = vec4(ouColor, 1.0);\n"
-"       return;\n"
-"   }\n"
-
-    // 1. Componente Ambiental
-	// Cambiar el 0.algo
-"   vec3 ambient = Ka * lightColor *2.0;\n"
-
-    // 2. Componente Difusa
-"   float diff = max(dot(norm, lightDir), 0.0);\n"
-"   vec3 diffuse = (diff * Kd) * lightColor * 1.0;\n"
-
-    // 3. Componente Especular
-"   vec3 viewDir = normalize(viewPos - FragPos);\n"
-"   vec3 reflectDir = reflect(-lightDir, norm);\n"
-"   float spec = pow(max(dot(viewDir, reflectDir), 0.0), Ns);\n"
-"   vec3 specular = (spec * Ks) * lightColor *1.0;\n"
-
-    // Combinar iluminación
-"   float distance = length(lightPos - FragPos);\n"
-"   float constant = 1.0;\n"
-"   float linear = 0.0001;\n"
-"   float quadratic = 0.00001;\n"
-"   float attenuation = 1.0 / (constant + linear * distance + quadratic * distance * distance);\n"
-"   ambient *= attenuation;\n"
-"   diffuse *= attenuation;\n"
-"   specular *= attenuation;\n"
-"   vec3 resultLight = ambient + diffuse + specular;\n"
-"   resultLight = max(resultLight, vec3(0.2));\n"
 "   if(useTexture){\n"
 "       vec4 texColor = texture(ouTexture, inOutUV);\n"
-"       outColor = texColor * vec4(resultLight, 1.0);\n"
-"   } else {\n"
-"       outColor = vec4(resultLight * ouColor, 1.0);\n"
-"   }\n"
-
+"       outColor = texColor * vec4(ouCol,1.0);\n"
+"	}\n"
+"   else{\n"
+"       outColor = vec4(ouCol,1.0);\n"
+"	}\n"
 "}\n";
 
 Shaders::Shaders(){
@@ -142,7 +83,7 @@ unsigned int Shaders::GetProgram(){
 }
 
 void Shaders::SetColor(const float &r,const float &g,const float &b){
-	int Color_loc=glGetUniformLocation(this->program_id,"ouColor");
+	int Color_loc=glGetUniformLocation(this->program_id,"ouCol");
 	
 	if(Color_loc <= -1){
 		std::cout << "ERROR or couldn't find the location :( " << std::endl;
@@ -227,28 +168,4 @@ void Shaders::SetUseTexture(bool value){
     }
 
     glUniform1i(loc,value);
-}
-
-void Shaders::SetMaterial(const float* Ka, const float* Kd, const float* Ks, float Ns) {
-    glUniform3fv(glGetUniformLocation(this->program_id, "Ka"), 1, Ka);
-    glUniform3fv(glGetUniformLocation(this->program_id, "Kd"), 1, Kd);
-    glUniform3fv(glGetUniformLocation(this->program_id, "Ks"), 1, Ks);
-    glUniform1f(glGetUniformLocation(this->program_id, "Ns"), Ns);
-}
-
-void Shaders::SetLight(const float* pos, const float* color, const float* viewPos) {
-    glUniform3fv(glGetUniformLocation(this->program_id, "lightPos"), 1, pos);
-    glUniform3fv(glGetUniformLocation(this->program_id, "lightColor"), 1, color);
-    glUniform3fv(glGetUniformLocation(this->program_id, "viewPos"), 1, viewPos);
-}
-
-void Shaders::SetIsLightCube(bool value){
-    int loc = glGetUniformLocation(program_id, "isLightCube");
-    
-    if(loc == -1){
-        // Puedes comentar el cout después de verificar que funciona para evitar spam
-        // std::cout << "Couldn't find isLightCube\n"; 
-        return;
-    }
-    glUniform1i(loc, value);
 }
